5cca53efe828df3fb3776b2aaa025040ce9e94df
i582 <i58215@voenmeh.ru>
2021-03-09 00:42:25 +0300

    cmd: add markdown output for stat

--- src/cmd/stat/main.go
+++ src/cmd/stat/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -35,6 +36,10 @@ type ReportCount struct {
 }
 
 func main() {
+	var markdown bool
+	flag.BoolVar(&markdown, "m", false, "print with markdown table")
+	flag.Parse()
+
 	reports := loadReportsFile("reports.json").Reports
 
 	var reportsByType = map[string]int{}
@@ -54,14 +59,31 @@ func main() {
 		return reportsCount[i].Count > reportsCount[j].Count
 	})
 
-	table := uitable.New()
+	if markdown {
+		fmt.Println("## The number of reports for tests.")
+		fmt.Printf("Count: %d\n\n", len(reports))
 
-	table.AddRow("Name", "Count")
+		fmt.Println("Name | Count")
+		fmt.Println("---- | :---:")
 
-	for _, info := range reportsCount {
-		table.AddRow(info.CheckName, info.Count)
+		for _, info := range reportsCount {
+			fmt.Printf("%s | %d (%.2f%%)\n", info.CheckName, info.Count, percent(info.Count, len(reports)))
+		}
+	} else {
+		table := uitable.New()
+
+		table.AddRow("Name", "Count")
+
+		for _, info := range reportsCount {
+			table.AddRow(info.CheckName, fmt.Sprintf("%d (%.2f%%)", info.Count, percent(info.Count, len(reports))))
+		}
+
+		fmt.Println("The number of reports for tests.")
+		fmt.Printf("Count: %d\n\n", len(reports))
+		fmt.Println(table)
 	}
+}
 
-	fmt.Printf("Count: %d\n\n", len(reports))
-	fmt.Println(table)
+func percent(x, y int) float64 {
+	return float64(x) / float64(y) * 100
 }
